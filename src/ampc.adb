53ac3cb0c6f1927e591799442bc97a5ae93c7321
Luke A. Guest <laguest@archeia.com>
2018-11-12 16:07:19 +0000

    Add Ada bindings to MPC lib.

---
+++ src/ampc.adb
@@ -0,0 +1,96 @@
+with Ada.Unchecked_Conversion;
+
+package body AMPC is
+   use type C.int;
+
+   function To_Result is new Ada.Unchecked_Conversion (Source => Values_Ptr, Target => Results_Ptr);
+
+   function Parse (Input    : in String;
+                   Parser   : in Parsers_Ptr;
+                   Result   : in Results_Ptr;
+                   Filename : in String := "<stdin>") return Boolean is
+      function Parse (Filename : in C.Strings.chars_ptr;
+                      Input    : in C.Strings.chars_ptr;
+                      Parser   : in Parsers_Ptr;
+                      Result   : in Results_Ptr) return C.int with
+        Import        => True,
+        Convention    => C,
+        External_Name => "mpc_parse";
+
+      C_Filename : C.Strings.chars_ptr := C.Strings.New_String (Filename);
+      C_Input    : C.Strings.chars_ptr := C.Strings.New_String (Input);
+      Success    : Boolean             := (Parse (C_Filename, C_Input, Parser, Result) = 1);
+   begin
+      C.Strings.Free (C_Filename);
+      C.Strings.Free (C_Input);
+
+      return Success;
+   end Parse;
+
+   function New_Parser (Name : in String) return Parsers_Ptr is
+      function New_Parser (Name : in C.Strings.chars_ptr) return Parsers_Ptr with
+        Import        => True,
+        Convention    => C,
+        External_Name => "mpc_new";
+
+      C_Name : C.Strings.chars_ptr := C.Strings.New_String (Name);
+      Result : Parsers_Ptr         := New_Parser (C_Name);
+   begin
+      C.Strings.Free (C_Name);
+
+      return Result;
+   end New_Parser;
+
+
+   procedure Free (Parser : in Parsers_Ptr) is
+      procedure Cleanup (Total : in Positive; Parser : in Parsers_Ptr) with
+        Import        => True,
+        Convention    => C,
+        External_Name => "mpc_cleanup";
+   begin
+      Cleanup (1, Parser);
+   end Free;
+
+   procedure Free (Parser_1 : in Parsers_Ptr;
+                   Parser_2 : in Parsers_Ptr) is
+      procedure Cleanup (Total    : in Positive;
+                         Parser_1 : in Parsers_Ptr;
+                         Parser_2 : in Parsers_Ptr) with
+        Import        => True,
+        Convention    => C,
+        External_Name => "mpc_cleanup";
+   begin
+      Cleanup (2, Parser_1, Parser_2);
+   end Free;
+
+   procedure Free (Parser_1 : in Parsers_Ptr;
+                   Parser_2 : in Parsers_Ptr;
+                   Parser_3 : in Parsers_Ptr) is
+      procedure Cleanup (Total    : in Positive;
+                         Parser_1 : in Parsers_Ptr;
+                         Parser_2 : in Parsers_Ptr;
+                         Parser_3 : in Parsers_Ptr) with
+        Import        => True,
+        Convention    => C,
+        External_Name => "mpc_cleanup";
+   begin
+      Cleanup (3, Parser_1, Parser_2, Parser_3);
+   end Free;
+
+   procedure Free (Parser_1 : in Parsers_Ptr;
+                   Parser_2 : in Parsers_Ptr;
+                   Parser_3 : in Parsers_Ptr;
+                   Parser_4 : in Parsers_Ptr) is
+      procedure Cleanup (Total    : in Positive;
+                         Parser_1 : in Parsers_Ptr;
+                         Parser_2 : in Parsers_Ptr;
+                         Parser_3 : in Parsers_Ptr;
+                         Parser_4 : in Parsers_Ptr) with
+        Import        => True,
+        Convention    => C,
+        External_Name => "mpc_cleanup";
+   begin
+      Cleanup (4, Parser_1, Parser_2, Parser_3, Parser_4);
+   end Free;
+end AMPC;
+
---
+++ src/ampc.ads
@@ -0,0 +1,143 @@
+with Interfaces.C.Strings;
+with System;
+
+package AMPC is
+   package C renames Interfaces.C;
+
+   --  States - mpc_state_t
+   type States is
+   record
+      Position : C.long;
+      Row      : C.long;
+      Column   : C.long;
+      Term     : C.int;
+   end record with
+     Convention => C;
+
+   type States_Ptr is access States with
+     Convention => C;
+
+   --  Errors - mpc_err_t
+   type Errors is
+   record
+      State        : States;
+      Expected_Num : C.int;
+      Filename     : C.Strings.chars_ptr;
+      Failure      : C.Strings.chars_ptr;
+      Expected     : System.Address;
+      Recieved     : C.char;
+   end record with
+     Convention => C;
+
+   type Errors_Ptr is access Errors with
+     Convention => C;
+
+   procedure Put (Error : in Errors_Ptr) with
+     Import        => True,
+     Convention    => C,
+     External_Name => "mpc_err_print";
+
+   procedure Free (Error : in Errors_Ptr) with
+     Import        => True,
+     Convention    => C,
+     External_Name => "mpc_err_delete";
+
+   --  Values - mpc_val_t
+   type Values is null record with
+     Convention => C;
+
+   type Values_Ptr is access Values;
+
+   type Results (Success : Boolean) is
+   record
+      case Success is
+         when False =>
+            Error : Errors_Ptr;
+
+         when True =>
+            Output : Values_Ptr;
+      end case;
+   end record with
+     Convention      => C_Pass_By_Copy,
+     Unchecked_Union => True;
+
+   type Results_Ptr is access all Results with
+     Convention => C;
+
+   --  Parsers - mpc_parser_t
+   type Parsers is null record with
+     Convention => C;
+
+   type Parsers_Ptr is access Parsers with
+     Convention => C;
+
+   function Parse (Input    : in String;
+                   Parser   : in Parsers_Ptr;
+                   Result   : in Results_Ptr;
+                   Filename : in String := "<stdin>") return Results;
+
+   function New_Parser (Name : in String) return Parsers_Ptr;
+
+   procedure Free (Parser : in Parsers_Ptr);
+   procedure Free (Parser_1 : in Parsers_Ptr;
+                   Parser_2 : in Parsers_Ptr);
+   procedure Free (Parser_1 : in Parsers_Ptr;
+                   Parser_2 : in Parsers_Ptr;
+                   Parser_3 : in Parsers_Ptr);
+   procedure Free (Parser_1 : in Parsers_Ptr;
+                   Parser_2 : in Parsers_Ptr;
+                   Parser_3 : in Parsers_Ptr;
+                   Parser_4 : in Parsers_Ptr);
+
+   type Language_Flags is (Default, Predictive, Whitespace_Sensitive) with
+     Convention => C;
+
+   function Language (Flags : in Language_Flags; Grammar : in String; Parser : in Parsers_Ptr) return Errors_Ptr with
+     Import        => True,
+     Convention    => C,
+     External_Name => "mpca_lang";
+
+   function Language (Flags    : in Language_Flags;
+                      Grammar  : in String;
+                      Parser_1 : in Parsers_Ptr;
+                      Parser_2 : in Parsers_Ptr) return Errors_Ptr with
+     Import        => True,
+     Convention    => C,
+     External_Name => "mpca_lang";
+
+   function Language (Flags    : in Language_Flags;
+                      Grammar  : in String;
+                      Parser_1 : in Parsers_Ptr;
+                      Parser_2 : in Parsers_Ptr;
+                      Parser_3 : in Parsers_Ptr) return Errors_Ptr with
+     Import        => True,
+     Convention    => C,
+     External_Name => "mpca_lang";
+
+   function Language (Flags    : in Language_Flags;
+                      Grammar  : in String;
+                      Parser_1 : in Parsers_Ptr;
+                      Parser_2 : in Parsers_Ptr;
+                      Parser_3 : in Parsers_Ptr;
+                      Parser_4 : in Parsers_Ptr) return Errors_Ptr with
+     Import        => True,
+     Convention    => C,
+     External_Name => "mpca_lang";
+
+   --  AST - mpc_ast_t
+   type ASTs is null record with
+     Convention => C;
+
+   type AST_Ptr is access ASTs with
+     Convention => C;
+
+   procedure Put (AST : in AST_Ptr) with
+     Import        => True,
+     Convention    => C,
+     External_Name => "mpc_ast_print";
+
+   procedure Free (AST : in AST_Ptr) with
+     Import        => True,
+     Convention    => C,
+     External_Name => "mpc_ast_delete";
+end AMPC;
