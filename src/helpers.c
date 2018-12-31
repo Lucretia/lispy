3601be04348bcfa20b5861b9f8b7d75beec94b70
Luke A. Guest <laguest@archeia.com>
2018-12-31 17:11:09 +0000

    Return correct type, mpc_err_t *. Actually return the value.

--- src/helpers.c
+++ src/helpers.c
@@ -1,39 +1,39 @@
 #include "../mpc/mpc.h"
 
-mpc_err_t helper_language2 (const int flags,
-                            const char *language,
-                            const mpc_parser_t *p1,
-                            const mpc_parser_t *p2)
+mpc_err_t *helper_language2 (const int flags,
+                             const char *language,
+                             const mpc_parser_t *p1,
+                             const mpc_parser_t *p2)
 {
-    mpca_lang (flags, language, p1, p2);
+    return mpca_lang (flags, language, p1, p2);
 }
 
-mpc_err_t helper_language3 (const int flags,
-                            const char *language,
-                            const mpc_parser_t *p1,
-                            const mpc_parser_t *p2,
-                            const mpc_parser_t *p3)
+mpc_err_t *helper_language3 (const int flags,
+                             const char *language,
+                             const mpc_parser_t *p1,
+                             const mpc_parser_t *p2,
+                             const mpc_parser_t *p3)
 {
-    mpca_lang (flags, language, p1, p2, p3);
+    return mpca_lang (flags, language, p1, p2, p3);
 }
 
-mpc_err_t helper_language4 (const int flags,
-                            const char *language,
-                            const mpc_parser_t *p1,
-                            const mpc_parser_t *p2,
-                            const mpc_parser_t *p3,
-                            const mpc_parser_t *p4)
+mpc_err_t *helper_language4 (const int flags,
+                             const char *language,
+                             const mpc_parser_t *p1,
+                             const mpc_parser_t *p2,
+                             const mpc_parser_t *p3,
+                             const mpc_parser_t *p4)
 {
-    mpca_lang (flags, language, p1, p2, p3, p4);
+    return mpca_lang (flags, language, p1, p2, p3, p4);
 }
 
-mpc_err_t helper_language5 (const int flags,
-                            const char *language,
-                            const mpc_parser_t *p1,
-                            const mpc_parser_t *p2,
-                            const mpc_parser_t *p3,
-                            const mpc_parser_t *p4,
-                            const mpc_parser_t *p5)
+mpc_err_t *helper_language5 (const int flags,
+                             const char *language,
+                             const mpc_parser_t *p1,
+                             const mpc_parser_t *p2,
+                             const mpc_parser_t *p3,
+                             const mpc_parser_t *p4,
+                             const mpc_parser_t *p5)
 {
-    mpca_lang (flags, language, p1, p2, p3, p4, p5);
+    return mpca_lang (flags, language, p1, p2, p3, p4, p5);
 }
